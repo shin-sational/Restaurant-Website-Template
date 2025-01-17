5efde4a6081c72f0adda7b131f8b3f27cf5d888b
yusuken1121 <yusuken1121@gmail.com>
2025-01-16 17:06:36 -0800

    ✨ Sectionタイトルのカスタムコンポーネントを作成。

---
+++ restaurant-website-template/src/components/common/atoms/CustomSectionTitle.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@/lib/utils";
+import React, { ReactNode } from "react";
+
+const CustomSectionTitle = ({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) => {
+  return (
+    <h2 className={cn("text-accent text-[40px] font-semibold", className)}>
+      {children}
+    </h2>
+  );
+};
+
+export default CustomSectionTitle;
