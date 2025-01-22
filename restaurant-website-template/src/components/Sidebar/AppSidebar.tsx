bd78ea958fa7c4f824ab1cd9e062eb99a4c5a967
yusuken1121 <yusuken1121@gmail.com>
2025-01-21 23:02:18 -0800

    ⚡️ instagramとfacebookのアイコンを追加。pathと一致するものに下線を追加。

--- restaurant-website-template/src/components/Sidebar/AppSidebar.tsx
+++ restaurant-website-template/src/components/Sidebar/AppSidebar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Sidebar,
   SidebarContent,
@@ -8,11 +9,36 @@ import {
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
+  useSidebar,
 } from "@/components/ui/sidebar";
 import { navigationLinks } from "@/config/frontend/siteConfig";
+import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+import CustomExternalLink from "../common/atoms/CustomExternalLink";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
 
-export function AppSidebar() {
+/**
+ * TODO
+ * Classnameの調整
+ * それぞれのアイテムの間隔を調整
+ * 選択時のエフェクトの調整
+ */
+
+export function AppSidebar({
+  instagramLink,
+  facebookLink,
+}: {
+  instagramLink?: string;
+  facebookLink?: string;
+}) {
+  const { setOpenMobile } = useSidebar();
+  const pathname = usePathname();
+  useEffect(() => {
+    setOpenMobile(false);
+  }, [pathname, setOpenMobile]);
+
   return (
     <Sidebar>
       <SidebarHeader className="hidden" />
@@ -23,11 +49,44 @@ export function AppSidebar() {
             <SidebarMenu>
               {navigationLinks.header.map((link) => (
                 <SidebarMenuItem key={link.href}>
-                  <SidebarMenuButton asChild>
-                    <Link href={link.href}>{link.label}</Link>
+                  <SidebarMenuButton
+                    asChild
+                    className="w-fit mx-auto flex flex-col items-center justify-center"
+                  >
+                    <Link
+                      href={link.href}
+                      className={cn(
+                        "rounded-none",
+                        pathname === link.href && "border-b border-accent"
+                      )}
+                    >
+                      {link.label}
+                    </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem className="flex items-center justify-center gap-4">
+                {instagramLink && (
+                  <SidebarMenuButton asChild className="w-fit">
+                    <CustomExternalLink href={instagramLink}>
+                      <FaInstagram />
+                    </CustomExternalLink>
+                  </SidebarMenuButton>
+                )}
+                {facebookLink && (
+                  <SidebarMenuButton asChild className="w-fit">
+                    <CustomExternalLink href={facebookLink}>
+                      <FaFacebook />
+                    </CustomExternalLink>
+                  </SidebarMenuButton>
+                )}
+              </SidebarMenuItem>
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
