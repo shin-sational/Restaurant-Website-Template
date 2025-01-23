711eba339f45411bfb603b1683132d853e22c4f4
yusuken1121 <yusuken1121@gmail.com>
2025-01-23 15:12:38 -0800

    ✨ menuのカテゴリのオブジェクトを作成し、追加。

--- restaurant-website-template/src/config/frontend/siteConfig.ts
+++ restaurant-website-template/src/config/frontend/siteConfig.ts
@@ -10,3 +10,12 @@ export const navigationLinks = {
     { label: "Contact", href: "/contact" },
   ],
 };
+
+export const MENU_CATEGORIES = [
+  { label: "APPETIZER", mainId: "#appetizer" },
+  { label: "MAIN", mainId: "#main" },
+  { label: "VEGGIES", mainId: "#veggies" },
+  { label: "SIDE DISH", mainId: "#sideDish" },
+  { label: "DRINK", mainId: "#drink" },
+  { label: "DESSERT", mainId: "#dessert" },
+];
