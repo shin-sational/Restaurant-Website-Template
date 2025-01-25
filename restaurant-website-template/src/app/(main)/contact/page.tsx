e2c283dc427773517ddafffecf9e9011219c4f6f
yusuken1121 <yusuken1121@gmail.com>
2025-01-24 23:01:20 -0800

    ⚡️ ContactページにFormを配置

--- restaurant-website-template/src/app/(main)/contact/page.tsx
+++ restaurant-website-template/src/app/(main)/contact/page.tsx
@@ -1,9 +1,19 @@
+import CustomSectionTitle from "@/components/common/atoms/CustomSectionTitle";
+import ContactForm from "@/components/Contact/ContactForm";
 import fetchPropertyData from "@/lib/fetchPropertyData";
 import React from "react";
 
 const Contact = async () => {
   const restaurantData = await fetchPropertyData();
-  return <div>contact</div>;
+  return (
+    // Contact Form
+    <div className="w-full px-[16px] py-[40px]">
+      <div className="flex flex-col items-center gap-[32px] w-full">
+        <CustomSectionTitle>Send Us Message</CustomSectionTitle>
+        <ContactForm />
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
