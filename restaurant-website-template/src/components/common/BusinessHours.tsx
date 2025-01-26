268c0e8054ef114ea62fa8f37155ec4d77117d49
Higesennine <yasuhito.komano@gmail.com>
2025-01-26 12:44:20 -0800

    BusinessHoursのclassNameを修正しました

--- restaurant-website-template/src/components/common/BusinessHours.tsx
+++ restaurant-website-template/src/components/common/BusinessHours.tsx
@@ -1,9 +1,10 @@
 import { OPENING_HOURS } from '@/config/frontend/siteConfig'
+import { cn } from '@/lib/utils'
 import React from 'react'
 
-const BusinessHours = () => {
+const BusinessHours = ({className}: {className?: string}) => {
   return (
-    <div className='h-auto flex flex-col gap-[2px] justify-center'>
+    <div className={cn('h-auto', 'flex flex-col', 'gap-[2px]', 'justify-center', className)}>
        {OPENING_HOURS.map(day => (
         <div key={day.day} className='grid grid-cols-2'>
           <div className='text-[12px]'>{day.day}</div>
