bf3b622230b509e4ac8f2447ad633b1f5a8faede
yusuken1121 <yusuken1121@gmail.com>
2025-01-30 18:52:14 -0800

    ⚡️ カテゴリに遷移するためのサブヘッダーをメニューページに挿入

--- restaurant-website-template/src/app/(main)/menu/page.tsx
+++ restaurant-website-template/src/app/(main)/menu/page.tsx
@@ -1,31 +1,26 @@
 import CustomSectionTitle from "@/components/common/atoms/CustomSectionTitle";
+import MenuSubHeader from "@/components/common/MenuSubHeader";
 import MenuItemCard from "@/components/Menu/MenuCard";
 import fetchPropertyData from "@/lib/fetchPropertyData";
-import { MenuItem } from "@/type/notionFrontend.Type";
+import { groupMenuItemsByCategory } from "@/utils/groupMenuItem";
 import React from "react";
 
 const Menu = async () => {
   const restaurantData = await fetchPropertyData();
 
-  /**
-   * カテゴリーごとにメニューをグループ化
-   * Appetizer:[MenuItem, MenuItem, ...],
-   * Main:[MenuItem, MenuItem, ...],
-   */
-  const groupedMenuItems: { [key: string]: MenuItem[] } = {};
-  restaurantData.Menu_Items.forEach((menuItem) => {
-    if (!groupedMenuItems[menuItem.Category]) {
-      groupedMenuItems[menuItem.Category] = [];
-    }
-    groupedMenuItems[menuItem.Category].push(menuItem);
-  });
+  const groupedMenuItems = groupMenuItemsByCategory(restaurantData);
 
   return (
     <section>
+      {/* カテゴリに遷移するためのサブヘッダー */}
+      <MenuSubHeader groupedMenuItems={groupedMenuItems} />
+
+      {/* カテゴリごとのメニューカード */}
       {Object.entries(groupedMenuItems).map(([category, menuItems]) => (
         <div
           key={category}
           className="flex flex-col items-center justify-center px-[16px] py-[32px] lg:px-[160px] lg:py-[80px] gap-[40px] lg:gap-[64px]"
+          id={category}
         >
           <CustomSectionTitle>{category}</CustomSectionTitle>
           <div className="grid lg:grid-cols-2 gap-[40px] lg:gap-[200px]">
