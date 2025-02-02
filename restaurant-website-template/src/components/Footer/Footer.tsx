3ea1d1d94b7715d1842dc2f568c7886ae972136a
yusuken1121 <yusuken1121@gmail.com>
2025-02-01 23:21:03 -0800

    ⚡️ Navigationのみクライアントコンポーネントに変更し、Footerを改善

--- restaurant-website-template/src/components/Footer/Footer.tsx
+++ restaurant-website-template/src/components/Footer/Footer.tsx
@@ -1,82 +1,51 @@
-"use client";
 import { RestaurantData } from "@/type/notionFrontend.Type";
-import React, { FC } from "react";
-import CustomImage from "../common/atoms/CustomImage";
-import { navigationLinks } from "@/config/frontend/siteConfig";
-import Link from "next/link";
+import React, { FC, memo } from "react";
 import { usePathname } from "next/navigation";
-import { cn } from "@/lib/utils";
-import CustomExternalLink from "../common/atoms/CustomExternalLink";
-import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { LogoSection } from "./LogoSection";
+import { FooterNavigation } from "./FooterNavigation";
+import { SNSLinks } from "./SnsLinks";
 
 /**
  * TODO
  * - Hoursを追加する
  * - Headerとクラスを統一できる場合は、utilityクラスを作って適用する
- * - レスポンシブ対応(モバイルのみ作成)
- * - logoがない時のハンドリング対応
  */
 
 interface Props {
   restaurantData: RestaurantData;
 }
 const Footer: FC<Props> = ({ restaurantData }) => {
-  const pathname = usePathname();
   const logo = restaurantData.Main_Branch_Info[0].Logo || null;
   const instagramLink = restaurantData.Main_Branch_Info[0].Instagram || null;
   const facebookLink = restaurantData.Main_Branch_Info[0].Facebook || null;
+
   return (
     <footer className="w-full bg-secondaryBackground px-[20px] md:px-[70px] py-[40px] md:py-[60px]">
       {/* モバイル版 */}
-      <div className="w-full flex flex-col items-center gap-[38px]">
-        {/* LOGO */}
-        {logo && (
-          <CustomImage
-            src={logo}
-            alt="restaurant logo"
-            ratio="16/9"
-            containerClassName="w-[84px]"
-          />
-        )}
-
-        {/* メニュー */}
-        <ul className="flex flex-col items-center gap-[12px]">
-          {navigationLinks.footer.map((link, index) => (
-            <li
-              key={index}
-              className={cn(
-                "text-[16px] text-foreground/80",
-                pathname === link.href &&
-                  "text-foreground font-bold border-b-2 border-accent"
-              )}
-            >
-              <Link href={link.href}>{link.label}</Link>
-            </li>
-          ))}
-        </ul>
-
-        {/* Hours */}
-
+      <div className="md:hidden w-full flex flex-col items-center gap-[38px]">
+        {/* ロゴ */}
+        <LogoSection logo={logo} containerClassName="max-w-[100px]" />
+        {/* ナビゲーション */}
+        <FooterNavigation />
         {/* SNSリンク */}
-        <ul className="flex items-center justify-center gap-[32px]">
-          {instagramLink && (
-            <li className="w-[24px] h-[24px]">
-              <CustomExternalLink href={instagramLink}>
-                <FaInstagram className="w-full h-full" />
-              </CustomExternalLink>
-            </li>
-          )}
-          {facebookLink && (
-            <li className="w-[24px] h-[24px]">
-              <CustomExternalLink href={facebookLink}>
-                <FaFacebook className="w-full h-full" />
-              </CustomExternalLink>
-            </li>
-          )}
-        </ul>
+        <SNSLinks instagramLink={instagramLink} facebookLink={facebookLink} />
+      </div>
+
+      {/* デスクトップ版 */}
+      <div className="hidden md:flex items-start justify-between">
+        <div className="flex flex-col justify-start gap-[40px]">
+          {/* ロゴ */}
+          <LogoSection logo={logo} containerClassName="max-w-[100px]" />
+          {/* SNSリンク */}
+          <SNSLinks instagramLink={instagramLink} facebookLink={facebookLink} />
+        </div>
+        {/* ナビゲーション */}
+        <FooterNavigation />
+        {/* 営業時間 */}
+        <div>Hoursをここに入れてください</div>
       </div>
     </footer>
   );
 };
 
-export default Footer;
+export default memo(Footer);
