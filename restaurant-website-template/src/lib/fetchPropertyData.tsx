63a0d0e2436a7d27b9be3934c1f400413c05b122
yusuken1121 <yusuken1121@gmail.com>
2025-02-02 23:01:14 -0800

    🐛

--- restaurant-website-template/src/lib/fetchPropertyData.tsx
+++ restaurant-website-template/src/lib/fetchPropertyData.tsx
@@ -1,4 +1,9 @@
-import { NEXT_PUBLIC_BASE_URL } from "@/config/ENV";
+import {
+  MAIN_BRANCH_INFO_DATABASE_ID,
+  MENU_ITEMS_DATABASE_ID,
+  NEXT_PUBLIC_BASE_URL,
+  SUB_BRANCHES_INFO_DATABASE_ID,
+} from "@/config/ENV";
 import { ERROR_MESSAGES } from "@/config/errorMessage";
 import { RestaurantData } from "@/type/notionFrontend.Type";
 
@@ -12,6 +17,17 @@ async function fetchPropertyData(): Promise<RestaurantData> {
     });
 
     if (!response.ok) {
+      if (
+        !MENU_ITEMS_DATABASE_ID ||
+        !MAIN_BRANCH_INFO_DATABASE_ID ||
+        !SUB_BRANCHES_INFO_DATABASE_ID
+      ) {
+        console.log("⭐️Database ID is missing:", {
+          MAIN_BRANCH_INFO_DATABASE_ID,
+          MENU_ITEMS_DATABASE_ID,
+          SUB_BRANCHES_INFO_DATABASE_ID,
+        });
+      }
       const errorMessage = await response.text(); // 追加: サーバーからのレスポンス内容を取得
       console.error(
         "Fetch failed:",
