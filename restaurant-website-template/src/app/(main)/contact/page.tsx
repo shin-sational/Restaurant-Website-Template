a2ffac82c9a3a2879b9338331fa6d5b2d751bdc0
yusuken1121 <yusuken1121@gmail.com>
2025-02-04 10:32:08 -0800

    🐛 fetchを分離して記述

--- restaurant-website-template/src/app/(main)/contact/page.tsx
+++ restaurant-website-template/src/app/(main)/contact/page.tsx
@@ -5,23 +5,24 @@ import fetchPropertyData from "@/lib/fetchPropertyData";
 import React from "react";
 
 const Contact = async () => {
-  const restaurantData = await fetchPropertyData();
+  // const restaurantData = await fetchPropertyData();
   return (
-    <section>
-      {/* ページタイトル */}
-      <PageTitleImage
-        backgroundImageSrc={restaurantData.Main_Branch_Info[0].Home_Menu_Photo3}
-        type="contact"
-      />
+    <div>contact</div>
+    // <section>
+    //   {/* ページタイトル */}
+    //   <PageTitleImage
+    //     backgroundImageSrc={restaurantData.Main_Branch_Info[0].Home_Menu_Photo3}
+    //     type="contact"
+    //   />
 
-      {/* Contact Form */}
-      <div className="w-full px-[16px] py-[40px]">
-        <div className="flex flex-col items-center gap-[32px] w-full">
-          <CustomSectionTitle>Send Us Message</CustomSectionTitle>
-          <ContactForm />
-        </div>
-      </div>
-    </section>
+    //   {/* Contact Form */}
+    //   <div className="w-full px-[16px] py-[40px]">
+    //     <div className="flex flex-col items-center gap-[32px] w-full">
+    //       <CustomSectionTitle>Send Us Message</CustomSectionTitle>
+    //       <ContactForm />
+    //     </div>
+    //   </div>
+    // </section>
   );
 };
 
--- restaurant-website-template/src/app/(main)/menu/page.tsx
+++ restaurant-website-template/src/app/(main)/menu/page.tsx
@@ -7,41 +7,43 @@ import { groupMenuItemsByCategory } from "@/utils/groupMenuItem";
 import React from "react";
 
 const Menu = async () => {
-  const restaurantData = await fetchPropertyData();
-
-  const groupedMenuItems = groupMenuItemsByCategory(restaurantData);
-
-  const handlePdfMenuClick = () => {
-    console.log("PDF Menu Clicked");
-  };
   return (
-    <section>
-      {/* ページタイトル */}
-      <PageTitleImage
-        backgroundImageSrc={restaurantData.Main_Branch_Info[0].Home_Menu_Photo2}
-        type="menu"
-        pdfLink={restaurantData.Main_Branch_Info[0].PDF_Menu}
-      />
+    <div>Menu</div>
+    // const restaurantData = await fetchPropertyData();
 
-      {/* カテゴリに遷移するためのサブヘッダー */}
-      <MenuSubHeader groupedMenuItems={groupedMenuItems} />
+    // const groupedMenuItems = groupMenuItemsByCategory(restaurantData);
 
-      {/* カテゴリごとのメニューカード */}
-      {Object.entries(groupedMenuItems).map(([category, menuItems]) => (
-        <div
-          key={category}
-          className="flex flex-col items-center justify-center px-[16px] py-[32px] lg:px-[160px] lg:py-[80px] gap-[40px] lg:gap-[64px]"
-          id={category}
-        >
-          <CustomSectionTitle>{category}</CustomSectionTitle>
-          <div className="grid lg:grid-cols-2 gap-[40px] lg:gap-[200px]">
-            {menuItems.map((menuItem) => (
-              <MenuItemCard menuItem={menuItem} key={menuItem.Name} />
-            ))}
-          </div>
-        </div>
-      ))}
-    </section>
+    // const handlePdfMenuClick = () => {
+    //   console.log("PDF Menu Clicked");
+    // };
+    // return (
+    //   <section>
+    //     {/* ページタイトル */}
+    //     <PageTitleImage
+    //       backgroundImageSrc={restaurantData.Main_Branch_Info[0].Home_Menu_Photo2}
+    //       type="menu"
+    //       pdfLink={restaurantData.Main_Branch_Info[0].PDF_Menu}
+    //     />
+
+    //     {/* カテゴリに遷移するためのサブヘッダー */}
+    //     <MenuSubHeader groupedMenuItems={groupedMenuItems} />
+
+    //     {/* カテゴリごとのメニューカード */}
+    //     {Object.entries(groupedMenuItems).map(([category, menuItems]) => (
+    //       <div
+    //         key={category}
+    //         className="flex flex-col items-center justify-center px-[16px] py-[32px] lg:px-[160px] lg:py-[80px] gap-[40px] lg:gap-[64px]"
+    //         id={category}
+    //       >
+    //         <CustomSectionTitle>{category}</CustomSectionTitle>
+    //         <div className="grid lg:grid-cols-2 gap-[40px] lg:gap-[200px]">
+    //           {menuItems.map((menuItem) => (
+    //             <MenuItemCard menuItem={menuItem} key={menuItem.Name} />
+    //           ))}
+    //         </div>
+    //       </div>
+    //     ))}
+    //   </section>
   );
 };
 
