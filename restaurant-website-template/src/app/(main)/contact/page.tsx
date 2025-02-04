1ae0db7c348ad4dee5fed8c23d3c27b2a3f485e8
yusuken1121 <yusuken1121@gmail.com>
2025-02-04 11:05:32 -0800

    ⚡️ FooterとContractページにfetchのラッパーを追加

--- restaurant-website-template/src/app/(main)/contact/page.tsx
+++ restaurant-website-template/src/app/(main)/contact/page.tsx
@@ -1,28 +1,32 @@
 import CustomSectionTitle from "@/components/common/atoms/CustomSectionTitle";
+import DataFetcher from "@/components/common/DataFetcher";
 import PageTitleImage from "@/components/common/PageTitleImage";
 import ContactForm from "@/components/Contact/ContactForm";
-import fetchPropertyData from "@/lib/fetchPropertyData";
 import React from "react";
 
 const Contact = async () => {
-  // const restaurantData = await fetchPropertyData();
   return (
-    <div>contact</div>
-    // <section>
-    //   {/* ページタイトル */}
-    //   <PageTitleImage
-    //     backgroundImageSrc={restaurantData.Main_Branch_Info[0].Home_Menu_Photo3}
-    //     type="contact"
-    //   />
+    <DataFetcher>
+      {(restaurantData) => (
+        <section>
+          {/* ページタイトル */}
+          <PageTitleImage
+            backgroundImageSrc={
+              restaurantData.Main_Branch_Info[0].Home_Menu_Photo3
+            }
+            type="contact"
+          />
 
-    //   {/* Contact Form */}
-    //   <div className="w-full px-[16px] py-[40px]">
-    //     <div className="flex flex-col items-center gap-[32px] w-full">
-    //       <CustomSectionTitle>Send Us Message</CustomSectionTitle>
-    //       <ContactForm />
-    //     </div>
-    //   </div>
-    // </section>
+          {/* Contact Form */}
+          <div className="w-full px-[16px] py-[40px]">
+            <div className="flex flex-col items-center gap-[32px] w-full">
+              <CustomSectionTitle>Send Us Message</CustomSectionTitle>
+              <ContactForm />
+            </div>
+          </div>
+        </section>
+      )}
+    </DataFetcher>
   );
 };
 
--- restaurant-website-template/src/components/Footer/Footer.tsx
+++ restaurant-website-template/src/components/Footer/Footer.tsx
@@ -1,9 +1,8 @@
-import { RestaurantData } from "@/type/notionFrontend.Type";
-import React, { FC, memo } from "react";
-import { usePathname } from "next/navigation";
+import React, { memo } from "react";
 import { LogoSection } from "./LogoSection";
 import { FooterNavigation } from "./FooterNavigation";
 import { SNSLinks } from "./SnsLinks";
+import DataFetcher from "../common/DataFetcher";
 
 /**
  * TODO
@@ -11,40 +10,50 @@ import { SNSLinks } from "./SnsLinks";
  * - Headerとクラスを統一できる場合は、utilityクラスを作って適用する
  */
 
-interface Props {
-  restaurantData: RestaurantData;
-}
-const Footer: FC<Props> = ({ restaurantData }) => {
-  const logo = restaurantData.Main_Branch_Info[0].Logo || null;
-  const instagramLink = restaurantData.Main_Branch_Info[0].Instagram || null;
-  const facebookLink = restaurantData.Main_Branch_Info[0].Facebook || null;
-
+const Footer = () => {
   return (
-    <footer className="w-full bg-secondaryBackground px-[20px] md:px-[70px] py-[40px] md:py-[60px]">
-      {/* モバイル版 */}
-      <div className="md:hidden w-full flex flex-col items-center gap-[38px]">
-        {/* ロゴ */}
-        <LogoSection logo={logo} containerClassName="max-w-[100px]" />
-        {/* ナビゲーション */}
-        <FooterNavigation />
-        {/* SNSリンク */}
-        <SNSLinks instagramLink={instagramLink} facebookLink={facebookLink} />
-      </div>
+    <DataFetcher>
+      {(restaurantData) => {
+        const logo = restaurantData.Main_Branch_Info[0].Logo || null;
+        const instagramLink =
+          restaurantData.Main_Branch_Info[0].Instagram || null;
+        const facebookLink =
+          restaurantData.Main_Branch_Info[0].Facebook || null;
+        return (
+          <footer className="w-full bg-secondaryBackground px-[20px] md:px-[70px] py-[40px] md:py-[60px]">
+            {/* モバイル版 */}
+            <div className="md:hidden w-full flex flex-col items-center gap-[38px]">
+              {/* ロゴ */}
+              <LogoSection logo={logo} containerClassName="max-w-[100px]" />
+              {/* ナビゲーション */}
+              <FooterNavigation />
+              {/* SNSリンク */}
+              <SNSLinks
+                instagramLink={instagramLink}
+                facebookLink={facebookLink}
+              />
+            </div>
 
-      {/* デスクトップ版 */}
-      <div className="hidden md:flex items-start justify-between">
-        <div className="flex flex-col justify-start gap-[40px]">
-          {/* ロゴ */}
-          <LogoSection logo={logo} containerClassName="max-w-[100px]" />
-          {/* SNSリンク */}
-          <SNSLinks instagramLink={instagramLink} facebookLink={facebookLink} />
-        </div>
-        {/* ナビゲーション */}
-        <FooterNavigation />
-        {/* 営業時間 */}
-        <div>Hoursをここに入れてください</div>
-      </div>
-    </footer>
+            {/* デスクトップ版 */}
+            <div className="hidden md:flex items-start justify-between">
+              <div className="flex flex-col justify-start gap-[40px]">
+                {/* ロゴ */}
+                <LogoSection logo={logo} containerClassName="max-w-[100px]" />
+                {/* SNSリンク */}
+                <SNSLinks
+                  instagramLink={instagramLink}
+                  facebookLink={facebookLink}
+                />
+              </div>
+              {/* ナビゲーション */}
+              <FooterNavigation />
+              {/* 営業時間 */}
+              <div>Hoursをここに入れてください</div>
+            </div>
+          </footer>
+        );
+      }}
+    </DataFetcher>
   );
 };
 
