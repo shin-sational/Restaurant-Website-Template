2a879e010e3c94cd6b89ee1ea0561edea3f8e323
Higesennine <yasuhito.komano@gmail.com>
2025-02-24 22:18:09 -0800

    ContactDetails responsiveness

--- restaurant-website-template/src/components/common/ContactDetails.tsx
+++ restaurant-website-template/src/components/common/ContactDetails.tsx
@@ -21,47 +21,72 @@ const ContactDetails = ({ restaurantData, className }: Props) => {
       {restaurantData.Main_Branch_Info.map((mainInfo) => (
         <div
           key={mainInfo.Name}
-          className="mt-[32px] mb-[32px] mr-[16px] ml-[16px]"
+          className="mt-[32px] mb-[32px] mr-[16px] ml-[16px] lg:mr-[160px] lg:ml-[160px]"
         >
           <CustomBranchName className="text-center mb-[16px]">
             {mainInfo.Name}
           </CustomBranchName>
-          <CustomImage
-            ratio="16/9"
-            src={mainInfo.Restaurant_Photo}
-            alt="restaurantImg"
-          />
-          <div>
-            <div className="mt-[16px] grid grid-cols-[18px_1fr] grid-rows-2 gap-[2px]">
-              <FaLocationDot className="w-[18px] h-[18px]" />
-              <h4 className="text-[14px]">Address</h4>
-              <p className="text-[12px] col-start-2 row-start-2">
-                {mainInfo.Address}
-              </p>
+          <div className="grid lg:grid-cols-2">
+            <div className="lg:hidden">
+              <CustomImage
+                ratio="16/9"
+                src={mainInfo.Restaurant_Photo}
+                alt="restaurantImg"
+              />
             </div>
-            <div className="mt-[16px] grid grid-cols-[18px_1fr] grid-rows-[21px_1fr] gap-[2px]">
-              <GoClock className="w-[18px] h-[18px] fill-white" />
-              <h4 className="text-[14px]">Hours</h4>
-              <div className="col-start-2 row-start-2 grid grid-cols-2">
-                <BusinessHours branchInfo={restaurantData.Main_Branch_Info} />
-              </div>
+            <div className="hidden lg:block">
+              <CustomImage
+                ratio="5/4"
+                src={mainInfo.Restaurant_Photo}
+                alt="restaurantImg"
+                containerClassName="max-w-[552px] max-h-[414px]"
+              />
             </div>
-            <div>
-              <div className="mt-[14px] grid grid-cols-[18px_1fr] grid-rows-2 gap-[2px]">
-                <FiPhone className="w-[18px] h-[18px] fill-white" />
-                <h4 className="text-[14px]">Phone</h4>
-                <p className="text-[12px] col-start-2 row-start-2">
-                  {mainInfo.Phone}
-                </p>
-              </div>
-            </div>
-            <div>
-              <div className="mt-[14px] grid grid-cols-[18px_1fr] grid-rows-2 gap-[2px]">
-                <CiMail className="w-[18px] h-[18px]" />
-                <h4 className="text-[14px]">Email</h4>
-                <p className="text-[12px] col-start-2 row-start-2">
-                  {mainInfo.Email}
-                </p>
+            <div className="lg:grid grid-cols-[1fr_5fr]">
+              <div></div>
+              <div>
+                <div className="mt-[16px] grid grid-cols-[18px_1fr] grid-rows-[auto_auto] gap-[2px]">
+                  <FaLocationDot className="w-[18px] h-[18px]" />
+                  <h4 className="text-[14px] lg:text-[24px] font-bold self-start">
+                    Address
+                  </h4>
+                  <p className="text-[12px] lg:text-[20px] col-start-2 row-start-2">
+                    {mainInfo.Address}
+                  </p>
+                </div>
+                <div className="mt-[16px] grid grid-cols-[18px_1fr] grid-rows-[auto_auto] gap-[2px]">
+                  <GoClock className="w-[18px] h-[18px] fill-white" />
+                  <h4 className="text-[14px] lg:text-[24px] font-bold self-start">
+                    Hours
+                  </h4>
+                  <div className="col-start-2 row-start-2 grid grid-cols-2 lg:grid-cols-none">
+                    <BusinessHours
+                      branchInfo={restaurantData.Main_Branch_Info}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="mt-[14px] grid grid-cols-[18px_1fr] grid-rows-[auto_auto] gap-[2px]">
+                    <FiPhone className="w-[18px] h-[18px] fill-white" />
+                    <h4 className="text-[14px] lg:text-[24px] font-bold self-start">
+                      Phone
+                    </h4>
+                    <p className="text-[12px] lg:text-[20px] col-start-2 row-start-2">
+                      {mainInfo.Phone}
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <div className="mt-[14px] grid grid-cols-[18px_1fr] grid-rows-[auto_auto] gap-[2px]">
+                    <CiMail className="w-[18px] h-[18px]" />
+                    <h4 className="text-[14px] lg:text-[24px] font-bold self-start">
+                      Email
+                    </h4>
+                    <p className="text-[12px] lg:text-[20px] col-start-2 row-start-2">
+                      {mainInfo.Email}
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -75,42 +100,67 @@ const ContactDetails = ({ restaurantData, className }: Props) => {
           <CustomBranchName className="text-center mb-[16px]">
             {subInfo.Name}
           </CustomBranchName>
-          <CustomImage
-            ratio="16/9"
-            src={subInfo.Restaurant_Photo}
-            alt="restaurantImg"
-          />
-          <div>
-            <div className="mt-[16px] grid grid-cols-[18px_1fr] grid-rows-2 gap-[2px]">
-              <FaLocationDot className="w-[18px] h-[18px]" />
-              <h4 className="text-[14px]">Address</h4>
-              <p className="text-[12px] col-start-2 row-start-2">
-                {subInfo.Address}
-              </p>
+          <div className="grid lg:grid-cols-2">
+            <div className="lg:hidden">
+              <CustomImage
+                ratio="16/9"
+                src={subInfo.Restaurant_Photo}
+                alt="restaurantImg"
+              />
             </div>
-            <div className="mt-[16px] grid grid-cols-[18px_1fr] grid-rows-[21px_1fr] gap-[2px]">
-              <GoClock className="w-[18px] h-[18px]" />
-              <h4 className="text-[14px]">Hours</h4>
-              <div className="col-start-2 row-start-2 grid grid-cols-2">
-                <BusinessHours branchInfo={restaurantData.Main_Branch_Info} />
-              </div>
+            <div className="hidden lg:block">
+              <CustomImage
+                ratio="5/4"
+                src={subInfo.Restaurant_Photo}
+                alt="restaurantImg"
+                containerClassName="max-w-[552px] max-h-[414px]"
+              />
             </div>
-            <div>
-              <div className="mt-[14px] grid grid-cols-[18px_1fr] grid-rows-2 gap-[2px]">
-                <FiPhone className="w-[18px] h-[18px] fill-white" />
-                <h4 className="text-[14px]">Phone</h4>
-                <p className="text-[12px] col-start-2 row-start-2">
-                  {subInfo.Phone}
-                </p>
-              </div>
-            </div>
-            <div>
-              <div className="mt-[14px] grid grid-cols-[18px_1fr] grid-rows-2 gap-[2px]">
-                <CiMail className="w-[18px] h-[18px]" />
-                <h4 className="text-[14px]">Email</h4>
-                <p className="text-[12px] col-start-2 row-start-2">
-                  {subInfo.Email}
-                </p>
+            <div className="lg:grid grid-cols-[1fr_5fr]">
+              <div></div>
+              <div>
+                <div className="mt-[16px] grid grid-cols-[18px_1fr] grid-rows-[auto_auto] gap-[2px]">
+                  <FaLocationDot className="w-[18px] h-[18px]" />
+                  <h4 className="text-[14px] lg:text-[24px] font-bold self-start">
+                    Address
+                  </h4>
+                  <p className="text-[12px] lg:text-[20px] col-start-2 row-start-2">
+                    {subInfo.Address}
+                  </p>
+                </div>
+                <div className="mt-[16px] grid grid-cols-[18px_1fr] grid-rows-[auto_auto] gap-[2px]">
+                  <GoClock className="w-[18px] h-[18px] fill-white" />
+                  <h4 className="text-[14px] lg:text-[24px] font-bold self-start">
+                    Hours
+                  </h4>
+                  <div className="col-start-2 row-start-2 grid grid-cols-2 lg:grid-cols-none">
+                    <BusinessHours
+                      branchInfo={restaurantData.Main_Branch_Info}
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="mt-[14px] grid grid-cols-[18px_1fr] grid-rows-[auto_auto] gap-[2px]">
+                    <FiPhone className="w-[18px] h-[18px] fill-white" />
+                    <h4 className="text-[14px] lg:text-[24px] font-bold self-start">
+                      Phone
+                    </h4>
+                    <p className="text-[12px] lg:text-[20px] col-start-2 row-start-2">
+                      {subInfo.Phone}
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <div className="mt-[14px] grid grid-cols-[18px_1fr] grid-rows-[auto_auto] gap-[2px]">
+                    <CiMail className="w-[18px] h-[18px]" />
+                    <h4 className="text-[14px] lg:text-[24px] font-bold self-start">
+                      Email
+                    </h4>
+                    <p className="text-[12px] lg:text-[20px] col-start-2 row-start-2">
+                      {subInfo.Email}
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
